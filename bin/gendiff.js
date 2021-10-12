3f5ee9da6e64f21fd7edcbff757b6282a4229975
MickRubashkin <mikhail.rubashkin@gmail.com>
2021-10-12 08:35:08 +0200

    add arguments and --format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
-import run from '../src/index.js';
+import { Command } from 'commander/esm.mjs';
 
-run();
+import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .version('0.1.0', '-V --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f --format [type]', 'output format')
+  // .action((filepath1, filepath2) => {
+  //   console.log(genDiff(filepath1, filepath2, program.opts().format))
+  // })
+  .parse();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import { Command } from 'commander/esm.mjs';
-
-export default () => {
-  const program = new Command();
-
-  program
-    .version('0.1.0')
-    .description('Compares two configuration files and shows a difference.')
-  // .arguments('<filepath1> <filepath2>')
-  // .option('-f --format [type]', 'output format')
-
-  program.parse();
+export default (filepath1, filepath2, options) => {
+  return 'Hey from genDiff!';
 };
