34c999600d0fb2630ca2686dfd1f9372bbd4e5e3
MickRubashkin <mikhail.rubashkin@gmail.com>
2021-12-23 02:28:40 +0200

    Move hardcoded diff from test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,128 +3,12 @@ import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
 import path, { dirname } from 'path';
 import gendiff from '../index.js';
+import diff from '../src/dummyDiffObj.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const diff = [
-  {
-    key: 'common',
-    type: 'nested',
-    children: [
-      {
-        key: 'follow',
-        type: 'added',
-        value: false,
-      },
-      {
-        key: 'setting1',
-        type: 'unchanged',
-        value: 'Value 1',
-      },
-      {
-        key: 'setting2',
-        type: 'removed',
-        value: 200,
-      },
-      {
-        key: 'setting3',
-        type: 'updated',
-        from: true,
-        to: null,
-      },
-      {
-        key: 'setting4',
-        type: 'added',
-        value: 'blah blah',
-      },
-      {
-        key: 'setting5',
-        type: 'added',
-        value: {
-          key5: 'value5',
-        },
-      },
-      {
-        key: 'setting6',
-        type: 'nested',
-        children: [
-          {
-            key: 'doge',
-            type: 'nested',
-            children: [
-              {
-                key: 'wow',
-                type: 'updated',
-                from: '',
-                to: 'so much',
-              },
-            ],
-          },
-          {
-            key: 'key',
-            type: 'unchanged',
-            value: 'value',
-          },
-          {
-            key: 'ops',
-            type: 'added',
-            value: 'vops',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    key: 'group1',
-    type: 'nested',
-    children: [
-      {
-        key: 'baz',
-        type: 'updated',
-        from: 'bas',
-        to: 'bars',
-      },
-      {
-        key: 'foo',
-        type: 'unchanged',
-        value: 'bar',
-      },
-      {
-        key: 'nest',
-        type: 'updated',
-        from: {
-          key: 'value',
-        },
-        to: 'str',
-      },
-    ],
-  },
-  {
-    key: 'group2',
-    type: 'removed',
-    value: {
-      abc: 12345,
-      deep: {
-        id: 45,
-      },
-    },
-  },
-  {
-    key: 'group3',
-    type: 'added',
-    value: {
-      deep: {
-        id: {
-          number: 45,
-        },
-      },
-      fee: 100500,
-    },
-  },
-];
 
 test('Generates diff recursively for json files', () => {
   const actual = JSON.parse(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json'));
